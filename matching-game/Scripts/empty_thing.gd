2a815cefb63ddbcfe52ef2483b12e76c1785e564
Yves <yvesprinz59@gmail.com>
2024-12-30 18:25:35 +0100

    Click a thing
    You can now click on a piece and the console will print out the position of the piece that got clicked on!

--- matching-game/Scripts/empty_thing.gd
+++ matching-game/Scripts/empty_thing.gd
@@ -2,7 +2,37 @@ extends Node2D
 
 var val = randi_range(0, 6)
 
+const BIG_LOADING = preload("res://sprites/Big-Loading.png")
+const BLURRY_JIM = preload("res://sprites/Blurry-Jim.png")
+const GREEN_PYTHON = preload("res://sprites/Green-Python.png")
+const LONG_SPIKE_FACTORY = preload("res://sprites/Long-SpikeFactory.png")
+const STRETCHED_AWESOME_FACE = preload("res://sprites/Stretched-awesome face.png")
+const TALL_ROBO_BOY = preload("res://sprites/Tall-Robo boy.png")
+const BLUE_BSM_2F_SQUID_BOSS = preload("res://sprites/Blue-BSM2F_Squid_Boss.png")
+
 func _ready() -> void:
 	print("I exist!")
-	print("children:")
-	print(get_child(0).name)
+	#print("children:")
+	#print(get_child(0).name)
+	print(val)
+	match val:
+		0: $Image.texture = BIG_LOADING
+		1: $Image.texture = BLURRY_JIM
+		2: $Image.texture = GREEN_PYTHON
+		3: $Image.texture = LONG_SPIKE_FACTORY
+		4: $Image.texture = STRETCHED_AWESOME_FACE
+		5: $Image.texture = TALL_ROBO_BOY
+		6: $Image.texture = BLUE_BSM_2F_SQUID_BOSS
+		
+
+
+func _input(event):
+	if event.is_action_pressed("grab"):
+		var mousePos = get_global_mouse_position()
+		if ((mousePos.x <= (global_position.x + 450)) && (mousePos.x >= (global_position.x - 450)) && (mousePos.y <= (global_position.y + 450)) && (mousePos.y >= (global_position.y - 450)) ):
+			print("click here! My position is: ", global_position, ", and the click was at: ", mousePos)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- matching-game/Scripts/grid.gd
+++ matching-game/Scripts/grid.gd
@@ -5,13 +5,7 @@ const gridSizeY = 7
 
 @onready var empty_thing: Node2D = $"Empty Thing"
 
-const BIG_LOADING = preload("res://sprites/Big-Loading.png")
-const BLURRY_JIM = preload("res://sprites/Blurry-Jim.png")
-const GREEN_PYTHON = preload("res://sprites/Green-Python.png")
-const LONG_SPIKE_FACTORY = preload("res://sprites/Long-SpikeFactory.png")
-const STRETCHED_AWESOME_FACE = preload("res://sprites/Stretched-awesome face.png")
-const TALL_ROBO_BOY = preload("res://sprites/Tall-Robo boy.png")
-const BLUE_BSM_2F_SQUID_BOSS = preload("res://sprites/Blue-BSM2F_Squid_Boss.png")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,18 +14,8 @@ func _ready() -> void:
 			print("duplicating...")
 			var gridPiece = empty_thing.duplicate()
 			add_child(gridPiece)
-			gridPiece.position.y = n * 900
-			gridPiece.position.x = i * 900
-			print(gridPiece.val)
-			match gridPiece.val:
-				0: gridPiece.get_node("Image").texture = BIG_LOADING
-				1: gridPiece.get_node("Image").texture = BLURRY_JIM
-				2: gridPiece.get_node("Image").texture = GREEN_PYTHON
-				3: gridPiece.get_node("Image").texture = LONG_SPIKE_FACTORY
-				4: gridPiece.get_node("Image").texture = STRETCHED_AWESOME_FACE
-				5: gridPiece.get_node("Image").texture = TALL_ROBO_BOY
-				6: gridPiece.get_node("Image").texture = BLUE_BSM_2F_SQUID_BOSS
-	pass # Replace with function body.
+			gridPiece.global_position.y = n * 900
+			gridPiece.global_position.x = i * 900
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
