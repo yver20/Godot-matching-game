507667d1f409438a4505204868b8990412685c00
Yves <yvesprinz59@gmail.com>
2025-08-14 22:26:59 +0200

    but for real this time
    restructured the pieces and their handling. Now, pretty much everything is done in the piece manager, rather some things in the piece script itself.

--- matching-game/Scripts/Piece.gd
+++ matching-game/Scripts/Piece.gd
@@ -6,7 +6,7 @@ var gridPos: Vector2
 #this is the X and Y positons 'on the grid' rather than its physical position in the world
 #This should be useful for debugging and double checking where matches are made
 
-var clicked = false
+var clicked: bool = false
 var savedPosition: Vector2 #This is where a new position for the currently held piece is stored, and it's applied when the user lets go.
 var oldPosition: Vector2 #This is the original position of the piece before moving it. Usefull to have as a default, and for swapping other pieces to it.
 
@@ -18,13 +18,11 @@ const STRETCHED_AWESOME_FACE = preload("res://sprites/Stretched-awesome face.png
 const TALL_ROBO_BOY = preload("res://sprites/Tall-Robo boy.png")
 const BLUE_BSM_2F_SQUID_BOSS = preload("res://sprites/Blue-BSM2F_Squid_Boss.png")
 
-func _ready() -> void:
+func _initialize_piece() -> void:
+	#This function is here to make sure the piece has the correct data after being created.
+	#Could and/or should be expanded as more qualities are added to pieces.
 	#print("I exist!")
 	savedPosition = global_position
-	#print("children:")
-	#print(get_child(0).name)
-	#print(val)
-	pieceType = randi_range(0,6)
 	match pieceType:
 		0: $Image.texture = BIG_LOADING
 		1: $Image.texture = BLURRY_JIM
@@ -35,42 +33,10 @@ func _ready() -> void:
 		6: $Image.texture = BLUE_BSM_2F_SQUID_BOSS
 		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("grab"):
-		var mousePos = get_global_mouse_position()
-		if ((mousePos.x <= (global_position.x + 450)) && (mousePos.x >= (global_position.x - 450)) && (mousePos.y <= (global_position.y + 450)) && (mousePos.y >= (global_position.y - 450)) ):
-			#print("click here! My position is: ", global_position, ", and the click was at: ", mousePos)
-			clicked = true
-			$Image.z_index = 1
-		savedPosition = global_position
-		oldPosition = global_position
-		
-	if Input.is_action_just_released("grab"):
-		if clicked:
-			global_position = savedPosition
-			$Image.z_index = 0
-			clicked = false
-		else:
-			global_position = savedPosition
-	if clicked:
-		global_position = get_global_mouse_position()
-		print("I'm clicked! saved position is: " + str(savedPosition))
-		
 
+# Forward collision signals to the manager
 func _on_area_entered(area: Area2D) -> void:
-	if clicked:
-		if area.is_in_group("tileColliders"):
-			print("area is in group 'tileColliders'!")
-			savedPosition = area.global_position
-		if area.is_in_group("pieces"):
-			area.savedPosition = oldPosition
-		
+	emit_signal("area_entered", self, area)
 
 func _on_area_exited(area: Area2D) -> void:
-	if clicked:
-		if area.is_in_group("tileColliders"):
-			print("area is in group 'tileColliders'!")
-			savedPosition = oldPosition
-		if area.is_in_group("pieces"):
-			area.savedPosition = area.oldPosition
+	emit_signal("area_exited", self, area)
--- matching-game/Scripts/pieceManager.gd
+++ matching-game/Scripts/pieceManager.gd
@@ -4,9 +4,11 @@ var gridSizeX: int
 var gridSizeY: int
 var spacing: int
 
-var pieces = [] # This will be board[y][x]
+var pieces = [] # This will be board[x][y]
+var currentlyDraggedPiece: Area2D = null
+var swappingPiece: Area2D
 
-@onready var piece: Area2D = $Piece
+@onready var pieceScene: Area2D = $Piece
 
 func _ready() -> void:
 	print("Piece manager exists")
@@ -19,14 +21,61 @@ func _generate_pieces() -> void:
 	for x in gridSizeX:
 		pieces[x] = []
 		for y in gridSizeY:
-			var currentPiece = piece.duplicate()
+			var currentPiece = pieceScene.duplicate()
 			add_child(currentPiece)
+			currentPiece.pieceType = randi_range(0,6)
 			currentPiece.add_to_group("pieces")
 			currentPiece.global_position = Vector2(x*spacing, y*spacing)
 			currentPiece.gridPos =  Vector2(x,y)
-			currentPiece.connect("area_entered", Callable(currentPiece, "_on_area_entered"))
-			currentPiece.connect("area_exited", Callable(currentPiece, "_on_area_exited"))
+			currentPiece._initialize_piece()
+			
+			# Input
+			currentPiece.connect("input_event", Callable(self, "_on_piece_input_event").bind(currentPiece))
+			# Collision forwarding
+			currentPiece.connect("area_entered", Callable(self, "_on_piece_area_entered").bind(currentPiece))
+			currentPiece.connect("area_exited", Callable(self, "_on_piece_area_exited").bind(currentPiece))
+			#current piece needs to be bound instead?
 			
 			pieces[x].append(currentPiece)
 #The above logic divides the grid in rows. Every array in the 'pieces' array is one row.
 #In other words, every 1st entry in one of the arrays of 'pieces' is part of the left most column, every second entry is part of the column to the right of that, and so on.
+
+
+func _on_piece_input_event(viewport: Node, event: InputEvent, shape_idx: int, piece: Area2D) -> void:
+	#print("bruh")
+	if event.is_action_pressed("grab"):
+		currentlyDraggedPiece = piece
+		piece.oldPosition = piece.global_position
+		piece.savedPosition = piece.global_position
+		piece.get_node("Image").z_index = 1
+
+	elif event.is_action_released("grab") and currentlyDraggedPiece:
+		#print("bruh")
+		currentlyDraggedPiece.global_position = currentlyDraggedPiece.savedPosition
+		currentlyDraggedPiece.get_node("Image").z_index = 0
+		currentlyDraggedPiece = null
+		if swappingPiece:
+			swappingPiece.global_position = swappingPiece.savedPosition
+			swappingPiece.oldPosition = global_position
+
+func _on_piece_area_entered(other: Area2D, piece: Area2D) -> void:
+	if currentlyDraggedPiece == piece:
+		if other.is_in_group("tileColliders"):
+			piece.savedPosition = other.global_position
+		elif other.is_in_group("pieces"):
+			# Swap positions
+			piece.savedPosition = other.oldPosition
+			other.savedPosition = piece.oldPosition
+			swappingPiece = other
+
+func _on_piece_area_exited(other: Area2D, piece: Area2D) -> void:
+	if currentlyDraggedPiece == piece:
+		if other.is_in_group("tileColliders"):
+			piece.savedPosition = piece.oldPosition
+		elif other.is_in_group("pieces"):
+			other.savedPosition = other.oldPosition
+			swappingPiece = null
+
+func _process(delta: float) -> void:
+	if currentlyDraggedPiece:
+		currentlyDraggedPiece.global_position = get_global_mouse_position()
