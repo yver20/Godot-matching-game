6ee62163a8a9ddc5fe61a81c59875c65941c2061
Yves <yvesprinz59@gmail.com>
2025-08-19 00:11:05 +0200

    Sprite overhaul
    Replaced all of the piece sprites with those from bejeweled, and added an 8th piece type. Also added more folders to the sprites, further subdividing the sprites.
    Also, apparently the max piece types was 6. whoops. Set it to 8 and set the default to 7.

--- matching-game/Scripts/Piece.gd
+++ matching-game/Scripts/Piece.gd
@@ -10,13 +10,14 @@ var clicked: bool = false
 var savedPosition: Vector2 #This is where a new position for the currently held piece is stored, and it's applied when the user lets go.
 var oldPosition: Vector2 #This is the original position of the piece before moving it. Usefull to have as a default, and for swapping other pieces to it.
 
-const BIG_LOADING = preload("res://sprites/Big-Loading.png")
-const BLURRY_JIM = preload("res://sprites/Blurry-Jim.png")
-const GREEN_PYTHON = preload("res://sprites/Green-Python.png")
-const LONG_SPIKE_FACTORY = preload("res://sprites/Long-SpikeFactory.png")
-const STRETCHED_AWESOME_FACE = preload("res://sprites/Stretched-awesome face.png")
-const TALL_ROBO_BOY = preload("res://sprites/Tall-Robo boy.png")
-const BLUE_BSM_2F_SQUID_BOSS = preload("res://sprites/Blue-BSM2F_Squid_Boss.png")
+const BLUE = preload("res://sprites/Bejeweled/blue_gem.png")
+const RED = preload("res://sprites/Bejeweled/red_gem.png")
+const GREEN = preload("res://sprites/Bejeweled/green_gem.png")
+const YELLOW = preload("res://sprites/Bejeweled/yellow_gem.png")
+const PURPLE = preload("res://sprites/Bejeweled/purple_gem.png")
+const ORANGE = preload("res://sprites/Bejeweled/orange_gem.png")
+const WHITE = preload("res://sprites/Bejeweled/white_gem.png")
+const  BLACK = preload("res://sprites/Bejeweled/black_gem.png")
 
 func _initialize_piece() -> void:
 	#This function is here to make sure the piece has the correct data after being created.
@@ -25,13 +26,16 @@ func _initialize_piece() -> void:
 	savedPosition = global_position
 	oldPosition = global_position
 	match pieceType:
-		0: $Image.texture = BIG_LOADING
-		1: $Image.texture = BLURRY_JIM
-		2: $Image.texture = GREEN_PYTHON
-		3: $Image.texture = LONG_SPIKE_FACTORY
-		4: $Image.texture = STRETCHED_AWESOME_FACE
-		5: $Image.texture = TALL_ROBO_BOY
-		6: $Image.texture = BLUE_BSM_2F_SQUID_BOSS
+		0: $Image.texture = BLUE
+		1: $Image.texture = RED
+		2: $Image.texture = GREEN
+		3: $Image.texture = YELLOW
+		4: $Image.texture = PURPLE
+		5: $Image.texture = ORANGE
+		6: $Image.texture = WHITE
+		7: $Image.texture = BLACK
+		_: $Image.texture = BLUE
+		
 
 # Forward collision signals to the manager
 func _on_area_entered(area: Area2D) -> void:
